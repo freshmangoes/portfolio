8afedb9518c09f056ab21727f070b180ef10178a
Kyle Cilia <kylecilia@icloud.com>
2020-09-02 19:16:51 -0700

    Updates

---
+++ precache-manifest.8a02b00c9cadcd36f627bc138f91156b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f811ab0fd6ac5696fd580f476b08f17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9e176bada133afa4818",
+    "url": "./static/css/main.5bd2e889.chunk.css"
+  },
+  {
+    "revision": "8a91f22ed70af20b91ab",
+    "url": "./static/js/2.55df1c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.55df1c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e176bada133afa4818",
+    "url": "./static/js/main.a7ef094b.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
